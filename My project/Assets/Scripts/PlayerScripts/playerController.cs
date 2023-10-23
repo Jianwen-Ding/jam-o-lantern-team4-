58d7472f4548278b9d7c0dd09ff6c7a8afcb1590
Jianwen Ding <jianwen_ding@icloud.com>
2023-10-23 01:11:39 -0400

    basic movement completed

--- My project/Assets/Scripts/PlayerScripts/playerController.cs
+++ My project/Assets/Scripts/PlayerScripts/playerController.cs
@@ -17,6 +17,8 @@ public class playerController : MonoBehaviour
     [SerializeField]
     float airSpeed;
     [SerializeField]
+    float midairReversalRatio;
+    [SerializeField]
     float jumpForce;
     //Checks whether midair or not
     [SerializeField]
@@ -43,7 +45,7 @@ public class playerController : MonoBehaviour
             float usedAngle = initialAngle + angle;
             Vector3 aug = new Vector3(distanceHorPerCheck * Mathf.Cos(usedAngle * Mathf.Deg2Rad), 0, distanceHorPerCheck * Mathf.Sin(usedAngle * Mathf.Deg2Rad));
             debugRay.createRay(gameObject.transform.position + aug, Vector3.down, distanceVerPerCheck, layerMask);
-            debugValueSys.display("" + angle, "" + usedAngle);  
+            //debugValueSys.display("" + angle, "" + usedAngle);  
             return Physics.Raycast(gameObject.transform.position + aug, Vector3.down, distanceVerPerCheck, layerMask);
         }
         bool leftTopCheck = checkAtAngle(45f);
@@ -52,7 +54,10 @@ public class playerController : MonoBehaviour
         bool rightBackCheck = checkAtAngle(-135f);
         return (centerCheck || leftTopCheck || rightTopCheck || leftBackCheck || rightBackCheck);
     }
-
+    private void OnCollisionExit(Collision collision)
+    {
+        midAir = true;
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (checkBeneath())
@@ -60,52 +65,50 @@ public class playerController : MonoBehaviour
             midAir = false;
         }
     }
-    private void OnCollisionStay(Collision collision)
-    {
-        if (checkBeneath())
-        {
-            midAir = false;
-        }
-    }
-    private void OnCollisionExit(Collision collision)
-    {
-        midAir = true;
-    }
     // Update is called once per frame
     void Update()
     {
         cameraHolder.transform.position = gameObject.transform.position;
         transform.rotation = Quaternion.Euler(0, getCam.yAngle, 0);
         float verInput = Input.GetAxisRaw("Verticle");
+        debugValueSys.display("verticle", "Verticle axis: " + verInput);
         float horInput = Input.GetAxisRaw("Horizontal");
+        debugValueSys.display("horizontal", "Horizontal axis: " + horInput);
         //walking and strafing midair
         if (verInput != 0 || horInput != 0)
         {
             float yAngleAdjust = 0;
+            //Foward
             if (verInput > 0 && horInput == 0)
             {
                 yAngleAdjust = 90;
             }
+            //Right Foward
             else if (verInput > 0 && horInput > 0)
             {
                 yAngleAdjust = 45;
             }
+            //Right
             else if (verInput == 0 && horInput > 0)
             {
                 yAngleAdjust = 0;
             }
+            //Back Right
             else if (verInput < 0 && horInput > 0)
             {
                 yAngleAdjust = -45;
             }
+            //Back
             else if (verInput < 0 && horInput == 0)
             {
                 yAngleAdjust = -90;
             }
+            //Back Left
             else if (verInput < 0 && horInput < 0)
             {
                 yAngleAdjust = -135;
             }
+            //Left
             else if (verInput == 0 && horInput < 0)
             {
                 yAngleAdjust = -180;
@@ -114,17 +117,28 @@ public class playerController : MonoBehaviour
             {
                 yAngleAdjust = 135;
             }
+            debugValueSys.display("adjust", "Adjust Angle: " + yAngleAdjust);
             float moveDir = yAngleAdjust - gameObject.transform.eulerAngles.y;
             //Strafing midair
             if (midAir)
             {
-                Vector3 moveAccel = new Vector3(airSpeed * Mathf.Cos(moveDir * Mathf.Deg2Rad), 0, airSpeed * Mathf.Sin(moveDir * Mathf.Deg2Rad));
+                float xMove = airSpeed * Mathf.Cos(moveDir * Mathf.Deg2Rad);
+                float zMove = airSpeed * Mathf.Sin(moveDir * Mathf.Deg2Rad);
+                if((xMove <= 0) != (objectPhysics.velocity.x <= 0))
+                {
+                    xMove *= midairReversalRatio;
+                }
+                if ((zMove <= 0) != (objectPhysics.velocity.z <= 0))
+                {
+                    zMove *= midairReversalRatio;
+                }
+                Vector3 moveAccel = new Vector3(xMove, 0, zMove);
                 objectPhysics.AddForce(moveAccel * Time.deltaTime, ForceMode.Acceleration);
             }
             //Walking on ground
             else
             {
-                Vector3 moveVel = new Vector3(groundSpeed * Mathf.Cos(moveDir * Mathf.Deg2Rad), objectPhysics.velocity.y, groundSpeed * Mathf.Sin(moveDir * Mathf.Deg2Rad));
+                Vector3 moveVel = new Vector3(groundSpeed * Mathf.Cos(moveDir * Mathf.Deg2Rad), 0, groundSpeed * Mathf.Sin(moveDir * Mathf.Deg2Rad));
                 objectPhysics.velocity = moveVel;
             }
 
@@ -134,8 +148,9 @@ public class playerController : MonoBehaviour
         //jumping
         if (spaInput != 0)
         {
-            if (midAir)
+            if (!midAir)
             {
+                midAir = true;
                 objectPhysics.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             }
         }
